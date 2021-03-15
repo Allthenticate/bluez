53b69bde484f4cedddb04fb09927a4ec2edbe08d
Bernie Conrad <charlie.conrad16@gmail.com>
2021-03-15 10:51:37 -0700

    WIP

--- reload.sh
+++ reload.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env sh
+
+# TODO requirements
+
+# Remove old bluez
 sudo make clean
 sudo systemctl stop bluetooth
 sudo make uninstall
-./configure --prefix=/usr --mandir=/usr/share/man --sysconfdir=/etc --localstatedir=/var --enable-library --enable-experimental --enable-deprecated --enable-maintainer-mode --with-dbusconfdir=/usr/share
+
+# Configure and make
+./configure --prefix=/usr --mandir=/usr/share/man --sysconfdir=/etc --localstatedir=/var --enable-library --enable-experimental --enable-deprecated --enable-maintainer-mode --with-dbusconfdir=/usr/share 2>errors.txt
 make -j 4
-sudo ./src/bluetoothd -n -P battery -d --experimental -f ./src/main.conf
-#sudo make install
-#sudo sed -i -e 's/ExecStart=.*/ExecStart=\/usr\/libexec\/bluetooth\/bluetoothd  -n -C -P battery -d --experimental /' /lib/systemd/system/bluetooth.service
-#sudo ../../scripts/update_bluez_config.sh /etc/dbus-1/system.d/bluetooth.conf
-#sudo systemctl daemon-reload
-#sudo systemctl unmask bluetooth
-#sudo systemctl restart bluetooth
+
+#sudo ./src/bluetoothd -n -P battery -d --experimental -f ./src/main.conf
+sudo make install
+sudo sed -i -e 's/ExecStart=.*/ExecStart=\/usr\/libexec\/bluetooth\/bluetoothd -C -P battery -d --experimental /' /lib/systemd/system/bluetooth.service
+
+# Get the proper configuration file
+sudo ../../scripts/update_bluez_config.sh src/bluetooth.conf
+sudo cp src/bluetooth.conf /etc/dbus-1/system.d/bluetooth.conf
+
+# Reload the config fle
+sudo systemctl daemon-reload
+sudo systemctl unmask bluetooth
+sudo systemctl restart bluetooth
